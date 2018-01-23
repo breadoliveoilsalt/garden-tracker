f54386ac19089639e16aa983404341d0c0371a8e
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-01-23 10:23:43 -0500

    Add partial to signup view that shows errors if any
    `

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
   has_secure_password
   # add validations HERE
   validates :name, presence: true, uniqueness: true#, message: "Error: You might not have provided a name, or your name might have been taken already."
-  validates :password, presence: true#, message: "Error: Please provide a password."
+  #validates :password, presence: true#, message: "Error: Please provide a password."
 
 end
