0571506c50bf9e9705a56e5706ea307179508d15
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-01-25 14:55:41 -0500

    Add migration for plantings table
    Also, add timestamps to migrations for plantings, gardens, and
    species.

--- db/migrate/20180125194005_create_species.rb
+++ db/migrate/20180125194005_create_species.rb
@@ -4,6 +4,7 @@ class CreateSpecies < ActiveRecord::Migration[5.1]
       t.string :name
       t.string :type
       t.string :sunlight
+      t.timestamps null: false
     end
   end
 end
---
+++ db/migrate/20180125194817_create_plantings.rb
@@ -0,0 +1,10 @@
+class CreatePlantings < ActiveRecord::Migration[5.1]
+  def change
+    create_table :plantings do |t|
+      t.integer :quantity
+      t.integer :garden_id
+      t.integer :species_id
+      t.timestamps null: false
+    end
+  end
+end
