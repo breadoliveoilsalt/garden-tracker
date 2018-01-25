837e7b38d1edd9b6a01b149ed1c272338215bd3f
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-01-25 15:05:59 -0500

    Create planting model

---
+++ app/models/planting.rb
@@ -0,0 +1,7 @@
+class Planting < ActiveRecord::Base
+
+  validates :quantity, presence: :true, numericality: {only_integer: true, greater_than: 0}
+  validates :garden_id, presence: :true, numericality: {only_integer: true}
+  validates :species_id, presence: :true, numericality: {only_integer: true}
+
+end
