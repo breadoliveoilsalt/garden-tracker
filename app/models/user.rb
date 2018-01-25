20ee94908feba03fe3bd767ee440c06fd428ec69
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-01-25 18:09:35 -0500

    Clean up migrations
    Dropped table.  Cleaned up create_users migration so that there is
    no longer a second migration to add github info -- ie, condenced
    two migrations. Moved create_plantings to come after
    create_garden_species.

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,10 +12,12 @@ class User < ActiveRecord::Base
   def self.create_user_from_github(auth)
     create! do |user|
       user.name = auth.extra.all_emails[0].email
-      user.password = SecureRandom.hex(10)
+      user.password = SecureRandom.hex(10) # has_secure_password requires a password. This generates random one.
+        # Double check that you don't need require 'securerandom' somewhere.
       user.provider = auth.provider
       user.uid = auth.uid
-      # user.oath_token = auth.credentials.token # Should I put this in bracket form like the others?
+      # user.oath_token = auth.credentials.token 
+        # Commented out line above b/c not sure why I'd need it.  Got it form one of those tutorials on line
     end
   end
 
--- db/migrate/20180121195806_create_users.rb
+++ db/migrate/20180121195806_create_users.rb
@@ -3,6 +3,9 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :name
       t.string :password_digest
+      t.string :provider
+      t.string :uid
+      t.string :oath_token
       t.timestamps null: false
     end
   end
