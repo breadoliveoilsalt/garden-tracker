45a5906e0b457dc1ce5108f777dd03bdeb019310
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-01-26 10:37:53 -0500

    Check that associations work with seed data
    Associations seem to work well.  Problem with species being both
    singular and plural seems to be cleared up given the language in
    config/initializers/inflections.rb and given the proper belongs_to
    associations in the species_garden model.

--- app/models/species_garden.rb
+++ app/models/species_garden.rb
@@ -3,7 +3,8 @@ class SpeciesGarden < ActiveRecord::Base
   validates :species_id, presence: true, numericality: {only_integer: true}
   validates :garden_id, presence: true, numericality: {only_integer: true}
 
-  belongs_to :garden
   belongs_to :species
+  belongs_to :garden
+
 
 end
