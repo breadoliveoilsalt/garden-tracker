2fab61bbb88b38d6ef012938267e4f8a10147659
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-01-26 12:02:35 -0500

    Add GardensController with new, show, create
    Also add gardens/show in view and resource :gardens in routes. All
    untested.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   root 'welcome#home'
 
-  resources :users
+  resources :users #probably have to modify at some point whether person can delete account etc.
 
   #get 'callback', to: 'sessions#create_from_github'
   get '/auth/:provider/callback', to: 'sessions#create_from_github'
@@ -11,5 +11,7 @@ Rails.application.routes.draw do
   get "/signin", to: "sessions#new"
   post "/sessions/create", to: "sessions#create"
   delete "/signout", to: "sessions#destroy"
+
+  resources :gardens #do I need only show, edit, etc.? 
 
 end
