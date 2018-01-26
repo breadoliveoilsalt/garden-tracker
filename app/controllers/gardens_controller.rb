98ef2d358e9ba516c7f21f7f64f341c689f83285
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-01-26 13:38:52 -0500

    Adds basic functionality to create a new garden
    In the middle of trying to make a custom error message if square
    feet are not accurately provided.

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -1,13 +1,30 @@
 class GardensController < ApplicationController
 
+# need a call back before each to make sure user is logged in and it is his or her stuff
+# you are allowed to look at.  Or else redirect with a flash message.
+
   def new
+    @garden = Garden.new
   end
 
   def create
-
+    @garden = current_user.gardens.build(garden_params)
+    if @garden.save
+      redirect_to garden_path(@garden.id)
+    else
+      raise @garden.errors.inspect
+    end
   end
 
   def show
-  end 
+    @garden = Garden.find_by(id: params[:id])
+  end
+
+
+  private
+
+  def garden_params
+    params.require(:garden).permit(:name, :description, :square_feet)
+  end
 
 end
