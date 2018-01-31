1a63468fea38a8c4a9312cd20c988634db7155f7
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-01-31 11:41:32 -0500

    Refactor custom writer in Garden model to take into account updates
    Seems to work.  In garden/edit view page, errors for garden validation
    appear on the view.  There is not an error for invalid planting quantity
    when editing, but it just stays the same.  Going to think about for a bit.

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -21,7 +21,6 @@ class GardensController < ApplicationController
   end
 
   def update
-    # binding.pry
     if @garden.update(garden_params)
       redirect_to garden_path(@garden.id)
     else
@@ -82,7 +81,7 @@ class GardensController < ApplicationController
   private
 
   def garden_params
-    params.require(:garden).permit(:name, :description, :square_feet, :user_id, plantings_attributes:[:species_id, :quantity, :user_id])
+    params.require(:garden).permit(:name, :description, :square_feet, :user_id, plantings_attributes:[:id, :species_id, :quantity, :user_id])
     # This is what I had to get new form to work.  Have to modify it b/c otherwise cant use with has_nested_attributes
     # params.require(:garden).permit(:name, :description, :square_feet, :user_id, planting_attributes: [:species_id, :quantity, :user_id])
   end
