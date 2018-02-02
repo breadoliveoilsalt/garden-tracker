cd6d2b5574c1020f1a077f2b6a0cd01adb9e03d3
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-02-02 10:05:40 -0500

    Add custom message to Planting model numericality validation for species_id

--- app/models/planting.rb
+++ app/models/planting.rb
@@ -2,7 +2,7 @@ class Planting < ActiveRecord::Base
 
   validates :quantity, presence: true, numericality: {only_integer: true, greater_than: 0}
   validates :garden_id, presence: true, numericality: {only_integer: true}
-  validates :species_id, presence: true, numericality: {only_integer: true}
+  validates :species_id, presence: true, numericality: {only_integer: true, message: "must be selected"}
   validates :user_id, presence: true, numericality: {only_integer: true}
 
   belongs_to :user
