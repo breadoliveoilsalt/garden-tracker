618f628b609200efe89c80d9927fc653997c424d
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-02-02 10:37:06 -0500

    Add passworld length validation to user class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
+
   has_secure_password
-  # add validations HERE
-  validates :name, presence: true, uniqueness: true#, message: "Error: You might not have provided a name, or your name might have been taken already."
-  #validates :password, presence: true#, message: "Error: Please provide a password."
+  validates :name, presence: true, uniqueness: true
+  validates :password, length: { in: 8..15 }
 
 
   has_many :gardens
