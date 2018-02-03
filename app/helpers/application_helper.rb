c5ee22836941deb9bfe7d90b5c606156ab8088de
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-02-03 12:49:48 -0500

    Modify helpers link_to_largest_garden and link_to_garden_with_most_plantings
    Modified so that if no gardens were created yet, a message
    to that effect appears.  Otherwise, an error message appeared.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,18 +7,25 @@ module ApplicationHelper
   end
 
   def link_to_largest_garden
-    largest_garden = Garden.order("square_feet DESC").first
-    link_to largest_garden.name, garden_path(largest_garden.id)
+    if Garden.any?
+      largest_garden = Garden.order("square_feet DESC").first
+      link_to largest_garden.name, garden_path(largest_garden.id)
+    else
+      "No gardens have been created yet."
+    end
   end
 
   def link_to_garden_with_most_plantings
-    garden = Garden.garden_with_most_plantings
-    link_to garden.name, garden_path(garden.id)
+    if Garden.any?
+      garden = Garden.garden_with_most_plantings
+      link_to garden.name, garden_path(garden.id)
+    else
+      "No gardens have been created yet."
+    end
   end
 
   def display_any_errors(object)
     render partial: 'layouts/errors', locals: {errors: object.errors} if object.errors.any?
-    # from new account - render partial: 'layouts/errors', locals: {errors: @user.errors} if @user.errors.any?
   end
-  
+
 end
