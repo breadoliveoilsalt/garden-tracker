0f5f5119dcbf094b294430d6fe1efbccd21385a0
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-04-28 19:26:12 -0400

    To user#show page, add working ajax request to species show link
    Right now, the json gets outputted to console.log

--- app/assets/javascripts/species.js
+++ app/assets/javascripts/species.js
@@ -4,12 +4,37 @@ $(function () {
 
 
 function attachListeners() {
-  $(".species_show_link").on("click", function() {
-      alert("show link clicked!")
+  $(".species_show_link").on("click", function(e) {
+
+      e.preventDefault()
+
+      let user_id = $(this).data().userId
+      let species_id = $(this).data().speciesId
+
+      $.ajax({
+        url: `/users/${user_id}/species/${species_id}.json`,
+        method: "GET"
+      }).done(function(data){
+        console.log(data)
+      })
   })
 }
 
-
+//
+//
+// $("#previous").on("click", function() {
+//       // To clear any previous buttons to make sure prior buttons are not re-loaded per test:
+//     $("#games").empty()
+//
+//     $.ajax({
+//       url: "/games",
+//       method: "GET"
+//     }).done(function(data) {
+//       data["data"].forEach( function (hash) {
+//         $("#games").append("<button id='game-" + hash["id"] + "' onclick='loadGame(this)' data-id='" + hash["id"] + "'>Game" + hash["id"] + "</button>")
+//         })
+//     })
+//   })
 
 // Add event listener to each of the links with the class
 // species-show.  Grab the data (which will be the species id)
