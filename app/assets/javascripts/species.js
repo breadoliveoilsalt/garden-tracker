6cab2316cdcee3cf263926f7b29ea2037ce2c900
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-04-28 09:22:31 -0400

    To user/show, add div for insertion of species display.
    Also add link with class to add event listener to.

--- app/assets/javascripts/species.js
+++ app/assets/javascripts/species.js
@@ -1,3 +1,9 @@
 $(function () {
     alert("doc ready!")
 })
+
+
+// Add event listener to each of the links with the class
+// species-show.  Grab the data (which will be the species id)
+// and then make the ajax request.  Don't forget to prevent
+//default
