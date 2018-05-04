bdf7e28a37716960b039e6671427beef3b57dfa9
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-05-03 22:26:26 -0400

    As part of rework, add function to garden.js to get gardenIds on page load
    This sets an array for userGardenIds when the page load, grabbing the user
    Id from the button...but that won't work, b/c the button shouldn't
    be available until after this array is set.

--- app/assets/javascripts/gardens.js
+++ app/assets/javascripts/gardens.js
@@ -1,17 +1,52 @@
+
+let userId
+let userGardenIds
+let userGardenIdsLength
+let indexOfCurrentGarden
+let counter = 0
+
+
 $(function() {
-  attachGardenListeners()
+  getUserGardensIds()
+  // attachGardenListeners()
 })
 
+
+function getUserGardensIds() {
+  userId = $(".next_garden_button").data().userId
+
+  $.ajax({
+        // Get the json representing the ids of a user's gardens
+      url: `/users/${userId}/get_garden_ids`,
+      method: "GET"
+    })
+
+        // Turn the json response into an array of the gardens ids
+    .then(function(data){
+      let arr = []
+      $(data).each(function (index, element){
+        arr.push(element["id"])
+      })
+      userGardenIds = arr
+      return arr
+    })
+    .then(function(arr) {
+      userGardenIds = arr
+      debugger
+    })
+
+}
   // A user's gardens will necessarily have ids in numerical order (e.g., a user's
   // gardens might be ids 2, 5, 7, 15).  So the first step in rendering a ajax request
   // is, on the first request, get all of the ids of the users gardens, to be an array
   // of user_garden_ids.  Then, on the subsequent ajax requests, the request will cycle
   // through this array to pull up the user's next garden.
 
-let userGardenIds
-let userGardenIdsLength
-let indexOfCurrentGarden
-let counter = 0
+  // NO: this is actually how to do it:  when the document loads, get the array of garden_ids;
+  // once it is there, then you can add the button!  That way I have it once, and the ajax request
+  // can still happen
+
+
 
 // This is getting too complicated.
 // Everytime: fetch the ids of the user's garden
@@ -19,42 +54,28 @@ let counter = 0
 // Then advance to the next element in the list
 // Then bring that up
 
-function attachGardenListeners() {
-  $(".next_garden_button").on("click", function(e) {
-
-        // not sure if i need this:
-      e.preventDefault()
-
-      let userId = $(this).data().userId
-      let gardenId = $(this).data().gardenId
-      let displayBox = $("#garden_display_id_" + gardenId)
-
-      $.ajax({
-            // Get the json representing the ids of a user's gardens
-          url: `/users/${userId}/get_garden_ids`,
-          method: "GET"
-        })
-
-            // Turn the json response into an array of the gardens ids
-        .then(function(data){
-          let arr = []
-          $(data).each(function (index, element){
-            arr.push(element["id"])
-          })
-          userGardenIds = arr
-          return arr
-        })
-
-        .then(function(arr) {
-          // make a function to test if current gardenId is the last element
-          // in the array. If so, then modify button to go back to beginning
-          // and add html.  If no, then modify the data of the button and
-          // add the html
-        })
-       })}
-          // return makeIdArray(data)
-
-          // indexOfCurrentGarden = getIndex(gardenId)
+// function attachGardenListeners() {
+//   $(".next_garden_button").on("click", function(e) {
+//
+//         // not sure if i need this:
+//       e.preventDefault()
+//
+//       let userId = $(this).data().userId
+//       let gardenId = $(this).data().gardenId
+//       let displayBox = $("#garden_display_id_" + gardenId)
+//
+//
+//
+//         .then(function(arr) {
+//           // make a function to test if current gardenId is the last element
+//           // in the array. If so, then modify button to go back to beginning
+//           // and add html.  If no, then modify the data of the button and
+//           // add the html
+//         })
+//        })}
+//           // return makeIdArray(data)
+//
+//           // indexOfCurrentGarden = getIndex(gardenId)
 
 
       // fetch(`/users/${userId}/get_garden_ids`)
@@ -101,9 +122,7 @@ function attachGardenListeners() {
 
 
 
-function getUserGardensIds(userId) {
 
-}
 
 // function makeIdArray(data) {
 //   let arr = []
