35a65280d82154fb5ab77a0405db7a9175423b46
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-05-21 20:30:08 -0400

    Add basic listener to user show page for show gardens link

--- app/assets/javascripts/gardens_user_show_page.js
+++ app/assets/javascripts/gardens_user_show_page.js
@@ -4,3 +4,21 @@
 // the link words with a list of the user's
 // gardens.
 // Have to format controller too...do I have respond_to for json vs html?
+
+
+// $(function() {
+//   attachGardensLinkListener()
+// })
+
+$(window).load(function() {
+  attachGardensLinkListener()
+})
+
+function attachGardensLinkListener() {
+
+  $('#view_gardens_link').on('click', function(e) {
+    e.preventDefault()
+    alert("yo!")
+  })
+
+}
