beb111c0588af8d4273f9dab878c01df9d5b88a2
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-05-22 14:21:25 -0400

    In gardens_for_user_show_page, remove old comments. About to merge into master.

--- app/assets/javascripts/gardens_for_user_show_page.js
+++ app/assets/javascripts/gardens_for_user_show_page.js
@@ -1,16 +1,3 @@
-// add listener to #view_gardens_link
-// the listener will then have a function
-// that gets the ajax request, and replaces
-// the link words with a list of the user's
-// gardens.
-// Have to format controller too...do I have respond_to for json vs html?
-
-
-// $(function() {
-//   attachGardensLinkListener()
-// })
-
-// import {Garden} from './gardens_for_gardens_show_page'
 
 $(window).load(function() {
   attachGardensLinkListener()
