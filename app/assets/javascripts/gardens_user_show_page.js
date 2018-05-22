c3fc6a622707de4b68fdeb124e3a6efb99505572
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-05-21 20:17:30 -0400

    Add to gardens_user_show_page.js: notes on next steps

---
+++ app/assets/javascripts/gardens_user_show_page.js
@@ -0,0 +1,6 @@
+// add listener to #view_gardens_link
+// the listener will then have a function
+// that gets the ajax request, and replaces
+// the link words with a list of the user's
+// gardens.
+// Have to format controller too...do I have respond_to for json vs html?
