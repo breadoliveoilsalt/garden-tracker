ac677f1604370ab3712aaf823e8ff45a238dc27d
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-11-18 10:25:50 -0500

    Remove comments re what did not work for active class status to menu buttons

--- app/assets/javascripts/menu_buttons.js
+++ app/assets/javascripts/menu_buttons.js
@@ -1,27 +1,3 @@
 $(function () {
   $(".top_menu").filter(function() {return this.href == location.href}).addClass("active").siblings().removeClass("active")
-  // const menuItems = $(".top_menu")
-  //
-  // for (let i = 0; i < menuItems.length; i++) {
-  //   menuItems[i].on("click", function(){
-  //     let current = $(".active")
-  //     debugger
-  //   })
-  // }
-
-  //   menuItems[i].addEventListener("click", function() {
-  //     let current = $(".active")
-  //     current[0].className = current[0].className.replace(" active", "")
-  //     this.className += " active"
-  //   });
-  // }
-
-  // $(".top_menu").on("click", function() {
-  //   debugger
-    // $(".active").removeClass("active")
-    // $(this).addClass("active")
-
-    // filter(function(){return this.href==location.href}).parent().addClass('active').siblings().removeClass('active')
-  // })
-
 })
