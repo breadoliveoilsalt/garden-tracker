053846d21ab8d77475d8650a1ab765c01a4f498f
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-11-19 10:23:18 -0500

    Fix new species migration so days_to_maturity is integer and add validation to species model

--- db/migrate/20181119002456_update_species.rb
+++ db/migrate/20181119002456_update_species.rb
@@ -1,6 +1,6 @@
 class UpdateSpecies < ActiveRecord::Migration[5.1]
   def change
     rename_column :species, :product, :category
-    add_column :species, :days_to_maturity, :date
+    add_column :species, :days_to_maturity, :integer
   end
 end
