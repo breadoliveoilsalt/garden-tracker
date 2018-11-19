98dbfaded27a5cef803ff80de45bf1948346fbea
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-11-18 19:01:27 -0500

    Revise menu_buttons.js so that if garden show page clicked, one of the menu buttons is no longer active

--- app/assets/javascripts/menu_buttons.js
+++ app/assets/javascripts/menu_buttons.js
@@ -1,3 +1,5 @@
 $(function () {
-  $(".top_menu").filter(function() {return this.href == location.href}).addClass("active").siblings().removeClass("active")
+  $(".top_menu active").removeClass("active")
+  $(".top_menu").filter(function() {return this.href == location.href}).addClass("active")
+  // $(".top_menu").filter(function() {return this.href == location.href}).addClass("active").siblings().removeClass("active")
 })
