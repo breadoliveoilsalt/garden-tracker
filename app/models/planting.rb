18d9f2ba69b95441257e5575940cc380b5b9e38b
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-11-20 10:26:25 -0500

    Clean up to date_planted and date_harvested migrations and custom validation methods.

--- app/models/planting.rb
+++ app/models/planting.rb
@@ -5,9 +5,23 @@ class Planting < ActiveRecord::Base
   validates :species_id, presence: true, numericality: {only_integer: true, message: "must be selected"}
   validates :user_id, presence: true, numericality: {only_integer: true}
 
+  validate :valid_date_planted?, :valid_date_harvested?
+
   belongs_to :user
   belongs_to :garden
   belongs_to :species
+
+  def valid_date_planted?
+    if date_planted.present? && !Date.valid_date?(date_planted.year, date_planted.month, date_planted.day)
+      errors.add(:date_planted, "is not a valid date")
+    end
+  end
+
+  def valid_date_harvested?
+    if date_harvested.present? && !Date.valid_date?(date_harvested.year, date_harvested.month, date_harvested.day)
+      errors.add(:date_harvested, "is not a valid date")
+    end
+  end
 
   def name
     self.species.name
