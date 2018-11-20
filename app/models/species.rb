68d99dd4b73018ca68edf20c2ebb39a0be646100
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-11-20 10:24:50 -0500

    Draft validations for date_planted and date_harvested. To be tested.

--- app/models/species.rb
+++ app/models/species.rb
@@ -23,9 +23,26 @@ class Species < ActiveRecord::Base
   validates :user_id, presence: true, numericality: {only_integer: true}
   validates :days_to_maturity, numericality: {only_integer: true}, allow_nil: true
 
+  validate :valid_date_planted?, :valid_date_harvested?
+
   belongs_to :user
   has_many :species_garden
   has_many :gardens, through: :species_garden
   has_many :plantings
+
+  def valid_date_planted?
+    if date_planted.present? && !Date.valid_date?(date_planted.year, date_planted.month, date_planted.day)
+      errors.add(:date_planted, "is not a valid date")
+    end
+  end
+
+  def valid_date_harvested?
+    if date_harvested.present?
+      if date_harvested.present? && !Date.valid_date?(date_harvested.year, date_harvested.month, date_harvested.day)
+        errors.add(:date_harvested, "is not a valid date")
+      end
+    end
+
+  end
 
 end
--- db/migrate/20181119191416_update_plantings.rb
+++ db/migrate/20181119191416_update_plantings.rb
@@ -1,6 +1,6 @@
 class UpdatePlantings < ActiveRecord::Migration[5.1]
   def change
     add_column :plantings, :date_planted, :date, default: Date.current
-    add_column :plantings, :harvested, :date
+    add_column :plantings, :date_harvested, :date
   end
 end
