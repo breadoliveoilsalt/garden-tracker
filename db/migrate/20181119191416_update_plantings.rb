1d86657d6b7327b99368cf9d73081ea0e160b154
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-12-08 17:51:44 -0500

    Remove default date from AR migration for plantings... problem is, the default was 'default: Date.current', and that would set the default date to the time of the migration.

--- db/migrate/20181119191416_update_plantings.rb
+++ db/migrate/20181119191416_update_plantings.rb
@@ -1,6 +1,6 @@
 class UpdatePlantings < ActiveRecord::Migration[5.1]
   def change
-    add_column :plantings, :date_planted, :date, default: Date.current
+    add_column :plantings, :date_planted, :date
     add_column :plantings, :date_harvested, :date
     add_column :plantings, :expected_maturity_date, :date
   end
