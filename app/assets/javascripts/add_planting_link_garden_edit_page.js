040cc18155010baec7115d65a2114152575e7a27
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-12-12 21:00:10 -0500

    Working on iifes: for add_planting_link.js, move all to document ready...
    ...jquery wrapper.  Works. Note that file name on first line is abbreviated.

--- app/assets/javascripts/add_planting_link_garden_edit_page.js
+++ app/assets/javascripts/add_planting_link_garden_edit_page.js
@@ -1,29 +1,32 @@
+
 $(function () {
   attachAddPlantingLinkListener()
+
+
+  function attachAddPlantingLinkListener() {
+    $("#add-planting-link").on("click", displayNewPlantingForm)
+  }
+
+  function displayNewPlantingForm(event) {
+    event.preventDefault()
+
+    const userId = event.target.dataset.userId
+    const gardenId = event.target.dataset.gardenId
+    const urlRequest = `/users/${userId}/gardens/${gardenId}/plantings/new`
+
+    $.ajax({
+      url: urlRequest,
+      method: `GET`
+    })
+    .then(function(response) {
+      const newPlantingForm = $(response).filter("#new-planting-form")[0].outerHTML
+      $("#new-planting-form-container")[0].innerHTML = newPlantingForm
+        // Remove link to add new garden
+      $("#add-planting-link-container")[0].innerHTML = ""
+      $([document.documentElement, document.body]).animate({
+         scrollTop: $("#new-planting-form-container").offset().top
+     }, 1000);
+    })
+  }
+
 })
-
-function attachAddPlantingLinkListener() {
-  $("#add-planting-link").on("click", displayNewPlantingForm)
-}
-
-function displayNewPlantingForm(event) {
-  event.preventDefault()
-
-  const userId = event.target.dataset.userId
-  const gardenId = event.target.dataset.gardenId
-  const urlRequest = `/users/${userId}/gardens/${gardenId}/plantings/new`
-
-  $.ajax({
-    url: urlRequest,
-    method: `GET`
-  })
-  .then(function(response) {
-    const newPlantingForm = $(response).filter("#new-planting-form")[0].outerHTML
-    $("#new-planting-form-container")[0].innerHTML = newPlantingForm
-      // Remove link to add new garden
-    $("#add-planting-link-container")[0].innerHTML = ""
-    $([document.documentElement, document.body]).animate({
-       scrollTop: $("#new-planting-form-container").offset().top
-   }, 1000);
-  })
-}
