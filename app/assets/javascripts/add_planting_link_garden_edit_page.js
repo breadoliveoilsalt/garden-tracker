e7fe5a4ecbe10e69717c152ca4d45b87eab53a45
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2018-12-12 21:29:27 -0500

    Complete iife for add_planting_link.js

--- app/assets/javascripts/add_planting_link_garden_edit_page.js
+++ app/assets/javascripts/add_planting_link_garden_edit_page.js
@@ -1,32 +1,42 @@
+// jQuery iife.  For formatting and reasoning, see here:
+// http://gregfranko.com/blog/jquery-best-practices/
 
-$(function () {
-  attachAddPlantingLinkListener()
+(
+  function(setupForNewPlanting){
 
+    setupForNewPlanting(window.jQuery, window, document);
 
-  function attachAddPlantingLinkListener() {
-    $("#add-planting-link").on("click", displayNewPlantingForm)
-  }
+  } (function($, window, document) {
 
-  function displayNewPlantingForm(event) {
-    event.preventDefault()
-
-    const userId = event.target.dataset.userId
-    const gardenId = event.target.dataset.gardenId
-    const urlRequest = `/users/${userId}/gardens/${gardenId}/plantings/new`
-
-    $.ajax({
-      url: urlRequest,
-      method: `GET`
+      // On document ready:
+    $(function () {
+      attachAddPlantingLinkListener()
     })
-    .then(function(response) {
-      const newPlantingForm = $(response).filter("#new-planting-form")[0].outerHTML
-      $("#new-planting-form-container")[0].innerHTML = newPlantingForm
-        // Remove link to add new garden
-      $("#add-planting-link-container")[0].innerHTML = ""
-      $([document.documentElement, document.body]).animate({
-         scrollTop: $("#new-planting-form-container").offset().top
-     }, 1000);
-    })
-  }
 
-})
+    function attachAddPlantingLinkListener() {
+      $("#add-planting-link").on("click", displayNewPlantingForm)
+    }
+
+    function displayNewPlantingForm(event) {
+      event.preventDefault()
+
+      const userId = event.target.dataset.userId
+      const gardenId = event.target.dataset.gardenId
+      const urlRequest = `/users/${userId}/gardens/${gardenId}/plantings/new`
+
+      $.ajax({
+        url: urlRequest,
+        method: `GET`
+      })
+      .then(function(response) {
+        const newPlantingForm = $(response).filter("#new-planting-form")[0].outerHTML
+        $("#new-planting-form-container")[0].innerHTML = newPlantingForm
+          // Remove link to add new garden
+        $("#add-planting-link-container")[0].innerHTML = ""
+        $([document.documentElement, document.body]).animate({
+           scrollTop: $("#new-planting-form-container").offset().top
+       }, 1000);
+      })
+    }
+  }
+))
